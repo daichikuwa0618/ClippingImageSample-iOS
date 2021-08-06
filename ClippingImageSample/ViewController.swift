ed2720ed00a808884659171470d9708d9d84cb35
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-08-06 12:38:02 +0900

    Initial Commit

---
+++ ClippingImageSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClippingImageSample
+//
+//  Created by Daichi Hayashi on 2021/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
